2832f420d690972f8f50f73b08eca0e951511f6c
Daniel <danielfrostdk@outlook.com>
2017-02-22 11:29:48 +0100

    init

---
+++ TennisGame/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace TennisGameDojo
+{
+    class Program
+    {
+        static void Main( string[] args ) {
+        }
+    }
+
+
+    public class Tennis
+    {
+        public List<TennisPlayer> Players { get; internal set; }
+        public List<PointHistory> PointHistoryList { get; set; }
+        public bool Deuce { get; private set; }
+        public int GameDelay { get; set; }
+        public bool IsInAdvantage { get; private set; }
+        public Guid PlayerInAdvantage { get; set; }
+        public bool MatchOver { get; set; }
+
+        public Tennis( List<TennisPlayer> playerLists ) {
+            if (playerLists == null)
+                throw new ArgumentNullException();
+
+            if (playerLists.Count != 2) {
+                throw new ArgumentException( "must be played by two players" );
+            }
+
+            foreach (var player in playerLists) {
+                player.GamePoints = new Point();
+                player.Identifier = Guid.NewGuid();
+            }
+
+            PointHistoryList = new List<PointHistory>();
+            Players = playerLists;
+        }
+
+
+        /// <summary>
+        /// This is where the actual game is going on.
+        /// </summary>
+        public void Play() {
+            //a game could take some time to find a point
+            SetDelay();
+
+            //find a winner amongst the players
+            PickGameWinner();
+
+            //we must update the winner points
+            SetGamePointForGameWinner();
+        }
+
+        public enum GamePointEnum { ZERO = 0, FIFTEEN = 15, THIRTY = 30, FORTY = 40 };
+
+        public void SetGamePointForGameWinner() {
+            var gameWinner = GetGameWinner();
+            var gameLoser = GetGameLoser();
+
+            switch (gameWinner.GamePoints.PointValue) {
+                case 0: {
+                    gameWinner.GamePoints.PointValue = (int)GamePointEnum.FIFTEEN;
+                    SetGamePointHistory( gameWinner, gameLoser, GamePointEnum.FIFTEEN );
+                    break;
+                }
+                case 15: {
+                    gameWinner.GamePoints.PointValue = (int)GamePointEnum.THIRTY;
+                    SetGamePointHistory( gameWinner, gameLoser, GamePointEnum.THIRTY );
+                    break;
+                }
+                case 30: {
+                    gameWinner.GamePoints.PointValue = (int)GamePointEnum.FORTY;
+                    SetGamePointHistory( gameWinner, gameLoser, GamePointEnum.FORTY );
+                    break;
+                }
+                case 40: {
+                    SetGamePointHistory( gameWinner, gameLoser, GamePointEnum.FORTY );
+                    UpdateScore();
+                    break;
+                }
+            }
+        }
+
+        public void ResetMatch() {
+            MatchOver = false;
+            PointHistoryList = new List<PointHistory>();
+        }
+
+
+        private void SetGamePointHistory( TennisPlayer winner, TennisPlayer loser, GamePointEnum point ) {
+            this.PointHistoryList.Add( new PointHistory { WinnerPoint = (int)point, LoserPoint = loser.GamePoints.PointValue } );
+        }
+
+        private void ResetGamePlayers() {
+            foreach (var player in Players) {
+                player.WonGame = false;
+                player.GamePoints.PointValue = 0;
+                player.Advantage = false;
+            }
+        }
+
+        private void ResetGame() {
+            Deuce = false;
+            IsInAdvantage = false;
+            PlayerInAdvantage = Guid.Empty;
+        }
+
+        private bool IsDeuce() {
+            Deuce = false;
+
+            var player1 = Players[0];
+            var player2 = Players[1];
+
+            if (player1.GamePoints.PointValue == 40) {
+                if (player2.GamePoints.PointValue == 40) {
+                    this.Deuce = true;
+                }
+            }
+
+            if (player2.GamePoints.PointValue == 40) {
+                if (player1.GamePoints.PointValue == 40) {
+                    this.Deuce = true;
+                }
+            }
+
+            if (this.Deuce) {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateScore() {
+            if (IsDeuce() && IsInAdvantage == false) {
+                SetAdvantage();
+                return;
+            }
+
+            if (IsInAdvantage) {
+                if (!IsAdvantagePlayerWinningGame()) {
+                    SetAdvantage();
+                    return;
+                }
+            }
+
+            SetPointsForWinner();
+            SetSetsWonForWinner();
+            IsMatchOver();
+
+            ResetGamePlayers();
+            ResetGame();
+        }
+
+        public void SetPointsForWinner() {
+            var winningPlayer = GetGameWinner();
+
+            winningPlayer.SetPoints++;
+        }
+
+        private void SetSetsWonForWinner() {
+            var winningPlayer = GetGameWinner();
+            var losingPlayer = GetGameLoser();
+
+            var setPointMargin = ( winningPlayer.SetPoints - losingPlayer.SetPoints );
+
+            if (setPointMargin >= 2) {
+                if (winningPlayer.SetPoints >= 6) {
+                    winningPlayer.SetsWon++;
+
+                    //reset setpoints
+                    winningPlayer.SetPoints = 0;
+                    losingPlayer.SetPoints = 0;
+                }
+            }
+        }
+
+        private void IsMatchOver() {
+            var winningPlayer = GetGameWinner();
+            var losingPlayer = GetGameLoser();
+
+            var setsWonMargin = ( winningPlayer.SetsWon - losingPlayer.SetsWon );
+
+            if (setsWonMargin >= 2) {
+                MatchOver = true;
+            } else {
+                MatchOver = false;
+            }
+        }
+
+        private bool IsAdvantagePlayerWinningGame() {
+            var winningPlayer = GetGameWinner();
+
+            if (winningPlayer.Identifier == PlayerInAdvantage) {
+                return true;
+            }
+
+            foreach (var player in Players) {
+                player.Advantage = false;
+            }
+
+            return false;
+        }
+
+        private void SetAdvantage() {
+            var winningPlayer = GetGameWinner();
+
+            winningPlayer.Advantage = true;
+            IsInAdvantage = true;
+            PlayerInAdvantage = winningPlayer.Identifier;
+        }
+
+        private void PickGameWinner() {
+            foreach (var player in Players) {
+                player.WonGame = false;
+            }
+
+            var gamerWinnerIndex = Tennis.RandomNumber( 0, 2 );
+
+            Players[gamerWinnerIndex].WonGame = true;
+        }
+
+        private TennisPlayer GetGameWinner() {
+            var gameWinner = Players.Where( p => p.WonGame == true ).FirstOrDefault();
+
+            if (gameWinner == null) {
+                throw new ArgumentException( "get game winner" );
+            }
+
+            return gameWinner;
+        }
+
+        private TennisPlayer GetGameLoser() {
+            var gameLoser = Players.Where( p => p.WonGame == false ).FirstOrDefault();
+
+            if (gameLoser == null) {
+                throw new ArgumentException( "get game loser" );
+            }
+
+            return gameLoser;
+        }
+
+        private void SetDelay() {
+            System.Threading.Thread.Sleep( Tennis.RandomNumber( 0, GameDelay ) );
+        }
+
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        private static int RandomNumber( int min, int max ) {
+            lock (syncLock) { // synchronize
+                return random.Next( min, max );
+            }
+        }
+    }
+
+    public class TennisPlayer
+    {
+        public Guid Identifier { get; set; }
+        public string Name { get; set; }
+        public Point GamePoints { get; set; }
+        public int SetPoints { get; set; }
+        public bool WonGame { get; internal set; }
+        public bool Advantage { get; internal set; }
+        public int SetsWon { get; set; }
+    }
+
+    public class PointHistory
+    {
+        public int WinnerPoint { get; set; }
+        public int LoserPoint { get; set; }
+    }
+
+
+    public class Point
+    {
+        public int PointValue { get; set; }
+    }
+}
---
+++ TennisGame/TennisTests.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisGameDojo;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FactoryDojo
+{
+    public class TennisTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public TennisTests( ITestOutputHelper outputHelper ) {
+            _outputHelper = outputHelper;
+        }
+
+
+        [Fact]
+        public void A_New_Game_Should_Have_Two_Players_With_0_Points() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            Assert.True( game.Players.Count == 2 );
+            Assert.True( game.Players.Select( x => x.GamePoints.PointValue == 0 ).Count() > 0 );
+        }
+
+        [Fact]
+        public void When_Game_On_Find_Point_For_Point_Winner() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Play();
+
+            Assert.True( game.Players.Where( p => p.WonGame == true ).Count() == 1 );
+            Assert.True( game.Players.Where( p => p.WonGame == false ).Count() == 1 );
+        }
+
+
+        [Fact]
+        public void Get_Game_Point_Winner_Points() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Play();
+
+            var winner = game.Players.Where( p => p.WonGame == true ).FirstOrDefault();
+
+            _outputHelper.WriteLine( winner.GamePoints.PointValue.ToString() );
+            Assert.True( winner.GamePoints.PointValue > 0 );
+        }
+
+        [Fact]
+        public void Players_Play_And_Points_Are_Scored() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() { Name = "Daniel" } );
+            playerLists.Add( new TennisPlayer() { Name = "Thomas" } );
+
+            var game = new Tennis( playerLists );
+
+            game.Play();
+            game.Play();
+            game.Play();
+            game.Play();
+            game.Play();
+
+            foreach (var player in game.Players) {
+                _outputHelper.WriteLine( player.GamePoints.PointValue.ToString() );
+            }
+
+            Assert.True( game.PointHistoryList.Count == 5 );
+            Assert.True( game.PointHistoryList.Count == 5 );
+        }
+
+
+        [Fact]
+        public void If_One_Player_Has_40_Points_And_The_Other_10_Set_A_SetPoint() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 10;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+            
+            foreach (var player in game.Players) {
+                _outputHelper.WriteLine( player.GamePoints.PointValue.ToString() );
+                _outputHelper.WriteLine( player.SetPoints.ToString() );
+            }
+
+            Assert.True( game.Players.Where( p => p.SetPoints > 0 ).FirstOrDefault() != null );
+        }
+
+        [Fact]
+        public void If_Playsers_Has_40_Game_Is_Deuce() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 40;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+
+            foreach (var player in game.Players) {
+                _outputHelper.WriteLine( player.GamePoints.PointValue.ToString() );
+                _outputHelper.WriteLine( player.SetPoints.ToString() );
+            }
+
+            Assert.True( game.Deuce );
+        }
+
+
+        [Fact]
+        public void If_Game_Is_Deuce_Winning_Player_Has_Advantage() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 40;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+            //deuce now
+
+            Assert.True( game.Players[0].Advantage == true );
+        }
+
+
+        [Fact]
+        public void If_Player_Has_Advantage_And_Wins_Game_He_Gets_Set_Point() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 40;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            //deuce
+            game.SetGamePointForGameWinner();
+
+            //advantage
+            game.SetGamePointForGameWinner();
+
+            Assert.True( game.Players[0].SetPoints == 1 );
+            Assert.True( game.Deuce == false );
+        }
+
+        [Fact]
+        public void If_Player_Has_6_SetPoints_And_Other_Player_Has_4_SetPoints_Player_With_6_SetPoints_Wins_Set() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].SetPoints = 5;
+            game.Players[1].SetPoints = 4;
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 15;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+
+            Assert.True( game.Players[0].SetPoints == 0 );
+            Assert.True( game.Players[0].SetsWon == 1 );
+        }
+
+
+        [Fact]
+        public void If_Player_Has_6_SetPoints_And_Other_Player_Has_5_And_Player_With_6_Points_Wins_He_Wins_Set() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].SetPoints = 6;
+            game.Players[1].SetPoints = 5;
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 15;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+
+            Assert.True( game.Players[0].SetPoints == 0 );
+            Assert.True( game.Players[0].SetsWon == 1 );
+        }
+
+        [Fact]
+        public void If_Player_Has_4_SetPoints_And_Other_Player_Has_3_No_One_Wins_Set() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].SetPoints = 4;
+            game.Players[1].SetPoints = 3;
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 15;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+
+            Assert.True( game.Players[0].SetPoints == 5 );
+            Assert.True( game.Players[0].SetsWon == 0 );
+        }
+
+        [Fact]
+        public void If_PLayer_Has_A_Margin_Of_2_Sets_Won_Player_Wins_Match() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            game.Players[0].SetPoints = 5;
+            game.Players[1].SetPoints = 3;
+
+            game.Players[0].SetsWon = 2;
+            game.Players[1].SetsWon = 1;
+
+            game.Players[0].GamePoints.PointValue = 40;
+            game.Players[1].GamePoints.PointValue = 15;
+
+            game.Players[0].WonGame = true;
+            game.Players[1].WonGame = false;
+
+            game.SetGamePointForGameWinner();
+
+            Assert.True( game.Players[0].SetsWon == 3 );
+            Assert.True( game.MatchOver );
+        }
+
+        [Fact]
+        public void Play_Until_A_Player_Wins_Match() {
+            var playerLists = new List<TennisPlayer>();
+            playerLists.Add( new TennisPlayer() );
+            playerLists.Add( new TennisPlayer() );
+
+            var game = new Tennis( playerLists );
+
+            while (!game.MatchOver) {
+                game.Play();
+            }
+
+            foreach (var item in game.PointHistoryList) {
+                _outputHelper.WriteLine(item.WinnerPoint + " - " + item.LoserPoint );
+            }
+        }
+    }
+}
