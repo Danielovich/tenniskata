8936a7b786176376b8a98fad74c037b97c0236bb
Daniel <danielfrostdk@outlook.com>
2017-03-10 09:11:41 +0000

    removed and renamed

--- TennisGame/TennisTests.cs
+++ TennisGame/TennisTests.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TennisGameDojo;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace FactoryDojo
+namespace TennisGameDojo
 {
     public class TennisTests
     {
